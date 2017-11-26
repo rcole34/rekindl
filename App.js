edd741b0155cd17cc872aab7b00ccf65c1641de9
jamescarroll <carrollj@stanford.edu>
2017-11-26 14:05:30 -0800

    changed reactnative project to most basic version

--- App.js
+++ App.js
@@ -6,10 +6,8 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload TESTING.</Text>
+        <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <View style={{flex: 1, backgroundColor: 'red'}}></View>
-
       </View>
     );
   }
