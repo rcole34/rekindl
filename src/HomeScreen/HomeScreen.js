8a424c47e4713eb815a57b6d5d5d3673764d4686
rcole34 <rcole34@stanford.edu>
2017-11-28 22:53:09 -0800

    made list items on home screen swipable

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Dimensions, View, Text, Button, FlatList, StyleSheet, Image, TouchableHighlight } from 'react-native';
+import Swipeout from 'react-native-swipeout'
 //var SearchBar = require('react-native-search-bar');
 var width = Dimensions.get('window').width;
+let swipeBtns = [{
+  text: 'High Five',
+  backgroundColor: 'green',
+  underlayColor: 'rgba(0, 0, 0, 0.6)'
+},
+{
+  text: 'Remove',
+  backgroundColor: 'red',
+  underlayColor: 'rgba(0, 0, 0, 0.6)'
+}];
 
 /*var allData=[
         {key: 'Claire R.', photo: require('../../assets/profilePictures/claire.png'), fire: require('../../assets/fires/smallFire.png'), lastConnected:"1 week ago"},
@@ -33,17 +44,19 @@ const HomeScreen = ({ navigation }) => (
         {key: 'Ella E.', photo: require('../../assets/profilePictures/ella.png'), fire: require('../../assets/fires/deadFire.png'), lastConnected:"2 weeks ago"},
       ]}
       
-      renderItem={({item}) => <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'
-        onPress={() => navigation.navigate('Detail', {name: item.key})}> 
-        <View style={{flex: 1, height: 100, width:width, flexDirection: 'row', justifyContent: 'center'}}>
-          <Image source={item.photo} style={{height:83, width:83, marginRight:10, marginTop:10, position:'absolute', left:10}}/>
-          <View style={{flexDirection: 'column', justifyContent: 'center', position:'absolute', left: 103, top:15}}>
-            <Text style={{fontSize: 42, color:'#444'}}>{item.key}</Text>
-            <Text style={{fontSize: 14, color:'#888'}}>last connected {item.lastConnected}</Text>
+      renderItem={({item}) => <Swipeout right={swipeBtns} backgroundColor= 'transparent' autoClose={true}>
+          <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'
+          onPress={() => navigation.navigate('Detail', {name: item.key})}> 
+          <View style={{flex: 1, height: 100, width:width, flexDirection: 'row', justifyContent: 'center'}}>
+            <Image source={item.photo} style={{height:83, width:83, marginRight:10, marginTop:10, position:'absolute', left:10}}/>
+            <View style={{flexDirection: 'column', justifyContent: 'center', position:'absolute', left: 103, top:15}}>
+              <Text style={{fontSize: 42, color:'#444'}}>{item.key}</Text>
+              <Text style={{fontSize: 14, color:'#888'}}>last connected {item.lastConnected}</Text>
+            </View>
+            <Image source={item.fire} style={{position:'absolute', right:0}}/>
           </View>
-          <Image source={item.fire} style={{position:'absolute', right:0}}/>
-        </View>
-      </TouchableHighlight>}
+        </TouchableHighlight>
+      </Swipeout>}
 
       ItemSeparatorComponent={this.renderSeparator}
     />
