949453c2e99f810e5bfe1c91b2c60e0abb36a46d
rcole34 <rcole34@stanford.edu>
2017-11-30 13:09:38 -0800

    Can edit user profile (name and bday)

--- src/ProfileScreen/EditProfileScreen.js
+++ src/ProfileScreen/EditProfileScreen.js
@@ -1,10 +1,85 @@
 import React from 'react';
-import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet, TouchableHighlight, Image, TextInput } from 'react-native';
+import DatePicker from 'react-native-datepicker'
 
-const EditProfileScreen = ({navigation}) => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>EditProfile</Text>
-  </View>
-);
+class EditProfileScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+        const navigation = this.props.navigation;
+        this.state = {user: navigation.state.params.user};
+    }
+
+    // pickImage() {
+    //     console.log(ImagePicker)
+    //     ImagePicker.openPicker({
+    //         width: 300,
+    //         height: 400,
+    //         cropping: true,
+    //         cropperCircleOverlay: true
+    //     }).then(image => {
+    //         // const user = Object.assign({}, this.state.user, { photo: require(image) }); 
+    //         // this.setState({ user });
+    //         console.log(image);
+    //     });
+    // }
+
+  render() {
+    const navigation = this.props.navigation;
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', flexDirection:'column', marginTop:'20%' }}>
+            <TouchableHighlight underlayColor='rgba(200,200,200,0.8)' style= {{height:150, width:150, borderRadius:150/2, marginBottom:20}} onPress = {() => {}}>
+                <Image source = {this.state.user.photo} style = {{alignItems: 'center', justifyContent: 'center', height:150, width:150}}>
+                    <View style={{alignItems: 'center', justifyContent: 'center', height:150, width:150, borderRadius:150/2, backgroundColor:'rgba(150,150,150,0.7)'}}>
+                        <Text>change profile picture</Text>
+                    </View>
+                </Image>
+            </TouchableHighlight>
+            <TextInput
+                style={{textAlign:'center', borderColor: 'gray', borderWidth:1, height: 64, fontSize:48}}
+                autoCapitalize='words'
+                placeholder="Enter name here"
+                value={this.state.user.name}
+                onChangeText={(text) => {
+                    const user = Object.assign({}, this.state.user, { name: text }); 
+                    this.setState({ user });
+                }}
+                returnKeyType='done'
+            />
+            <View style={{alignItems: 'center', justifyContent: 'center', flexDirection:'row', marginTop:15}}>
+                <Text style = {{fontSize:24, color:'#666'}}>Birthday: </Text>
+                <DatePicker 
+                    date='string' 
+                    format='MMMM D, YYYY' 
+                    confirmBtnText='Done' 
+                    cancelBtnText='Cancel' 
+                    showIcon={false} 
+                    date={this.state.user.birthday} 
+                    onDateChange={(date) => {
+                        const user = Object.assign({}, this.state.user, { birthday: date }); 
+                        this.setState({ user });
+                    }} 
+                    customStyles={{
+                        dateText:{fontSize:18, color:'#666'}
+                    }}
+                    style={{width:200}}/>
+            </View>
+            
+
+            <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'
+                style={{position:'absolute', right:20, bottom:20, height:64, width:64, borderRadius:64/2}}
+                onPress={() => {
+                    navigation.state.params.onSave(this.state.user);
+                    navigation.goBack();
+                }}> 
+                <View style={{alignItems: 'center', justifyContent:'center', flexDirection:'column', backgroundColor:'#EE4948',height:64, width:64, borderRadius:64/2, shadowColor: '#000000', shadowOffset: {width: 0, height: 4}, shadowRadius: 4, shadowOpacity: 0.7}}>
+                    <Text style={{color:'#FFF', fontSize:18}}>Save</Text>
+                </View>
+            </TouchableHighlight>
+        </View>
+    );
+  }
+}
+  
 
 export default EditProfileScreen;
--- src/ProfileScreen/ProfileScreen.js
+++ src/ProfileScreen/ProfileScreen.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { View, Text, Image, TouchableHighlight } from 'react-native';
 
-var user = {name: 'Michael West', photo: require('../../assets/profilePictures/mike.png'), birthMonth: 'December', birthDay: 5};
 
 class ProfileScreen extends React.Component {
+  constructor(props) {
+        super(props);
+        const navigation = this.props.navigation;
+        this.state = {user: {name: 'Michael West', photo: require('../../assets/profilePictures/mike.png'), birthday: 'Decmber 5, 1996'}}
+    }
   static navigationOptions = {
     tabBarLabel: 'Profile',
     tabBarIcon: ({ tintColor }) => {
@@ -14,21 +18,26 @@ class ProfileScreen extends React.Component {
     },
   };
 
+  onSave = user => {
+    console.log('On save called! with user', user);
+    this.setState({user: user});
+  };
+
 
 
   render() {
-    const { navigate } = this.props.navigation;
+    const navigation = this.props.navigation;
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', flexDirection:'column' }}>
-        <Image source = {user.photo} style = {{height:150, width:150}}/>
+        <Image source = {this.state.user.photo} style = {{height:150, width:150}}/>
         <View style={{flexDirection:'row', alignItems: 'center'}}>
-          <Text style = {{fontSize:48, color:'#444'}}>{user.name}</Text>
+          <Text style = {{fontSize:48, color:'#444'}}>{this.state.user.name}</Text>
           <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'
-          onPress={() => navigate('EditProfile', user)}>
+          onPress={() => navigation.navigate('EditProfile', {user: this.state.user, onSave: this.onSave})}>
             <Image source={require('../../assets/icons/edit.png')} style={{height:30, width:30, tintColor:'#555', marginLeft:10}}/>
           </TouchableHighlight>
         </View>
-        <Text style = {{fontSize:24, color:'#666'}}>Birthday: {user.birthMonth} {user.birthDay}</Text>
+        <Text style = {{fontSize:24, color:'#666'}}>Birthday: {this.state.user.birthday}</Text>
         <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'>
           <View style={{flexDirection:'row', alignItems:'center', marginTop:20}}>
             <Image source={require('../../assets/icons/facebook.png')} style={{height:20, width:20, marginRight:15}}/>
@@ -36,7 +45,7 @@ class ProfileScreen extends React.Component {
           </View>
         </TouchableHighlight>
         <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'
-        onPress={() => navigate('Schedule', user)}> 
+        onPress={() => navigation.navigate('Schedule', this.state.user)}> 
           <View style={{flexDirection: 'column', alignItems:'center', justifyContent:'center', marginTop:20}}>
             <Image source={require('../../assets/icons/calendar.png')} style={{height:80, width:76, marginBottom:5}}/>
             <Text style={{fontSize:18, color:'#444'}}>Update Schedule</Text>
