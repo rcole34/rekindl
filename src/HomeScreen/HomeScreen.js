43ca89d084588f87dbf9140e0c6f078ec998fb32
rcole34 <rcole34@stanford.edu>
2017-12-04 12:33:40 -0800

    made notifications show up but problems with them going away

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -15,15 +15,15 @@ class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {allData: [
-        {key: 1, name: 'Claire R.', photo: require('../../assets/profilePictures/claire.png'), fire: require('../../assets/fires/small_fire.png'), lastConnected:"1 week ago"},
-        {key: 2, name:'John S.', photo: require('../../assets/profilePictures/john.png'), fire: require('../../assets/fires/large_fire.png'), lastConnected:"yesterday"},
-        {key: 3, name:'Nate G.', photo: require('../../assets/profilePictures/nate.png'), fire: require('../../assets/fires/medium_fire.png'), lastConnected:"4 days ago"},
-        {key: 4, name:'Ella E.', photo: require('../../assets/profilePictures/ella.png'), fire: require('../../assets/fires/dead_fire.png'), lastConnected:"2 weeks ago"}
+        {key: 1, name: 'Claire R.', photo: require('../../assets/profilePictures/claire.png'), fire: require('../../assets/fires/small_fire.png'), lastConnected:"1 week ago", lastConnectionType:"High-Fived", notificationCount:1},
+        {key: 2, name:'John S.', photo: require('../../assets/profilePictures/john.png'), fire: require('../../assets/fires/large_fire.png'), lastConnected:"yesterday", lastConnectionType:"Sent Text", notificationCount:0},
+        {key: 3, name:'Nate G.', photo: require('../../assets/profilePictures/nate.png'), fire: require('../../assets/fires/medium_fire.png'), lastConnected:"4 days ago", lastConnectionType:"Added Memory", notificationCount:0},
+        {key: 4, name:'Ella E.', photo: require('../../assets/profilePictures/ella.png'), fire: require('../../assets/fires/dead_fire.png'), lastConnected:"2 weeks ago", lastConnectionType:"High-Fived", notificationCount:0}
       ], currData: [
-        {key: 1, name:'Claire R.', photo: require('../../assets/profilePictures/claire.png'), fire: require('../../assets/fires/small_fire.png'), lastConnected:"1 week ago"},
-        {key: 2, name:'John S.', photo: require('../../assets/profilePictures/john.png'), fire: require('../../assets/fires/large_fire.png'), lastConnected:"yesterday"},
-        {key: 3, name:'Nate G.', photo: require('../../assets/profilePictures/nate.png'), fire: require('../../assets/fires/medium_fire.png'), lastConnected:"4 days ago"},
-        {key: 4, name:'Ella E.', photo: require('../../assets/profilePictures/ella.png'), fire: require('../../assets/fires/dead_fire.png'), lastConnected:"2 weeks ago"}
+        {key: 1, name:'Claire R.', photo: require('../../assets/profilePictures/claire.png'), fire: require('../../assets/fires/small_fire.png'), lastConnected:"1 week ago", lastConnectionType:"High-Fived", notificationCount:1},
+        {key: 2, name:'John S.', photo: require('../../assets/profilePictures/john.png'), fire: require('../../assets/fires/large_fire.png'), lastConnected:"yesterday", lastConnectionType:"Sent Text", notificationCount:0},
+        {key: 3, name:'Nate G.', photo: require('../../assets/profilePictures/nate.png'), fire: require('../../assets/fires/medium_fire.png'), lastConnected:"4 days ago", lastConnectionType:"Added Memory", notificationCount:0},
+        {key: 4, name:'Ella E.', photo: require('../../assets/profilePictures/ella.png'), fire: require('../../assets/fires/dead_fire.png'), lastConnected:"2 weeks ago", lastConnectionType:"High-Fived", notificationCount:0}
       ], width : Dimensions.get('window').width};
     this._removeFriend = this._removeFriend.bind(this)
     this._removeFriendPressed = this._removeFriendPressed.bind(this)
@@ -38,7 +38,6 @@ _removeFriend = (item) => {
       }
     };
     this.setState({currData : this.state.allData});
-    console.log(this.state.currData);
   }
 
   _removeFriendPressed(item) {
@@ -61,6 +60,17 @@ _removeFriend = (item) => {
     };
     currData = newData;
   }*/
+  _removeNotifications(key) {
+    dataCopy = this.state.currData;
+    for (var i = 0; i < this.state.currData.length; i++) {
+      if(this.state.currData[i].key === key) {
+        dataCopy[i].notificationCount = 0;
+        this.setState({ currData : dataCopy });
+        this.setState({ allData : dataCopy });
+      }
+    };
+    
+  }
 
   _renderItem(item, navigation) {
     let swipeBtns = [{
@@ -78,17 +88,23 @@ _removeFriend = (item) => {
     return(
           <Swipeout right={swipeBtns} backgroundColor= 'transparent' autoClose={true}>
             <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'
-            onPress={() => navigation.navigate('Detail', {name: item.name, photo: item.photo, fire: item.fire, lastConnected: item.lastConnected})}> 
-            <View style={{flex: 1, height: 100, width:this.state.width, flexDirection: 'row', justifyContent: 'center'}}>
-              <Image source={item.photo} style={{height:83, width:83, marginRight:10, marginTop:10, position:'absolute', left:10}}/>
-              <View style={{flexDirection: 'column', justifyContent: 'center', position:'absolute', left: 103, top:15}}>
-                <Text style={{fontSize: 42, color:'#444'}}>{item.name}</Text>
-                <Text style={{fontSize: 14, color:'#888'}}>last connected {item.lastConnected}</Text>
+            onPress={() => {
+              this._removeNotifications(item.key);
+              navigation.navigate('Detail', {name: item.name, photo: item.photo, fire: item.fire, lastConnected: item.lastConnected});
+            }}> 
+              <View style={{flex: 1, height: 100, width:this.state.width, flexDirection: 'row', justifyContent: 'center'}}>
+                <Image source={item.photo} style={{height:83, width:83, marginRight:10, marginTop:10, position:'absolute', left:10}}/>
+                <View style={{display:item.notificationCount==0?'none':'flex', alignItems: 'center', justifyContent:'center', flexDirection:'column', backgroundColor:'#EE4948',height:26, width:26, borderRadius:26/2, position:'absolute', top:7, left:7}}>
+                  <Text style={{color:'#FFF', fontSize:14,}}>{item.notificationCount}</Text>
+                </View>
+                <View style={{flexDirection: 'column', justifyContent: 'center', position:'absolute', left: 103, top:15}}>
+                  <Text style={{fontSize: 42, color:'#444', fontWeight:item.notificationCount==0?'normal':'bold'}}>{item.name}</Text>
+                  <Text style={{fontSize: 14, color:'#888'}}>{item.lastConnectionType} {item.lastConnected}</Text>
+                </View>
+                <Image source={item.fire} style={{position:'absolute', right:0, width: 95, height: 95}}/>
               </View>
-              <Image source={item.fire} style={{position:'absolute', right:0, width: 95, height: 95}}/>
-            </View>
-          </TouchableHighlight>
-        </Swipeout>);
+            </TouchableHighlight>
+          </Swipeout>);
   }
 
   render() {
