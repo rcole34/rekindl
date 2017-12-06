127a9ef067a649c0aef5c658e01d2e7e4a439150
aplim <aplim@stanford.edu>
2017-12-05 19:33:57 -0800

    ,
    Done

--- src/MemoriesScreen/MemoriesNavigator.js
+++ src/MemoriesScreen/MemoriesNavigator.js
@@ -3,6 +3,8 @@ import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation'; // 1.0.0-beta.14
 import MemoriesScreen from './MemoriesScreen.js';
 import AddMemoryScreen from './AddMemoryScreen.js'
+import MemoryDetailScreen from './MemoryDetailScreen.js'
+
 
 const RootNavigator = StackNavigator({
   Memories: {
@@ -16,6 +18,13 @@ const RootNavigator = StackNavigator({
     screen: AddMemoryScreen,
     navigationOptions: ({navigation}) => ({
       title: "Add Memory",
+    }),
+  },
+
+  MemoryDetail: {
+    screen: MemoryDetailScreen,
+    navigationOptions: ({navigation}) => ({
+      title: "Change Me"
     }),
   },
 });
--- src/MemoriesScreen/MemoriesScreen.js
+++ src/MemoriesScreen/MemoriesScreen.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import { View, Text, TouchableHighlight, FlatList, Image } from 'react-native';
-
+import { View, Text, Button, Picker, TouchableHighlight, FlatList, Image } from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown'
 
 class MemoriesScreen extends React.Component {
+
   constructor(props) {
     super(props);
     this.state={ memories: [
-      {key: '1', photo: require('../../assets/memories/hiking.png')}
-    ]};
+      {key: '1', photo: require('../../assets/memories/hiking.png')},
+      {key: '2', photo: require('../../assets/memories/hiking.png')},
+      {key: '3', photo: require('../../assets/memories/hiking.png')},
+      {key: '4', photo: require('../../assets/memories/hiking.png')},
+      {key: '5', photo: require('../../assets/memories/hiking.png')},
+      {key: '6', photo: require('../../assets/memories/hiking.png')},
+      {key: '7', photo: require('../../assets/memories/hiking.png')},
+      {key: '9', photo: require('../../assets/memories/hiking.png')},
+      {key: '10', photo: require('../../assets/memories/hiking.png')},
+      {key: '11', photo: require('../../assets/memories/hiking.png')},
+      {key: '12', photo: require('../../assets/memories/hiking.png')},
+      {key: '13', photo: require('../../assets/memories/hiking.png')},
+      {key: '14', photo: require('../../assets/memories/hiking.png')},
+      {key: '15', photo: require('../../assets/memories/hiking.png')},
+      {key: '16', photo: require('../../assets/memories/hiking.png')},
+      {key: '17', photo: require('../../assets/memories/hiking.png')},
+      {key: '18', photo: require('../../assets/memories/hiking.png')},
+      {key: '19', photo: require('../../assets/memories/hiking.png')},
+      {key: '20', photo: require('../../assets/memories/hiking.png')}
+    ], user: "java"};
   }
 
  _renderItem(item, navigation) {
     console.log(item)
     return(
-      <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'>
-        <View style={{flex: 1, height: 100, width:100, flexDirection: 'row', justifyContent: 'center'}}>
-            <Image source={item.photo} style={{height:83, width:83, marginRight:10, marginTop:10, position:'absolute', left:10}}/>
+      <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'
+        onPress={() => navigation.navigate('MemoryDetail')}>
+        <View style={{flex: 1, height: 105, width: 105, marginLeft: 10, flexDirection: 'row', justifyContent: 'center'}}>
+            <Image source={item.photo} style={{height:105, width:105, marginLeft:0, position:'absolute', left: 10}}/>
         </View>
       </TouchableHighlight>
   );
@@ -24,39 +44,61 @@ class MemoriesScreen extends React.Component {
   render() {
     const { navigate } = this.props.navigation;
     return(
-      <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'center' }}>
-        <FlatList
-          visible={this.state.memories.length!==0}
-          data={this.state.memories}
-          renderItem={({item}) => this._renderItem(item, {navigate})}
 
-          ItemSeparatorComponent={this.renderSeparator}
-        />
+      <View style={{ flex: 1 }}>
 
-      <Image source={this.state.memories.photo} style={{height:83, width:83, marginRight:10, marginTop:10, position:'absolute', left:10}}/>
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center'}}>      
+          <Text style={{fontSize: 25, marginTop: 25 }}> Memories With: </Text>
 
-      <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'
-        onPress={() => navigate('AddMemory')} style={{position:'absolute', right:20, bottom:20, height:64, width:64, borderRadius:64/2}}> 
-          <View style={{alignItems: 'center', justifyContent:'center', flexDirection:'column', backgroundColor:'#EE4948',height:64, width:64, borderRadius:64/2, shadowColor: '#000000', shadowOffset: {width: 0, height: 4}, shadowRadius: 4, shadowOpacity: 0.7}}>
-            <Text style={{color:'#FFF', fontSize:32, marginBottom:5}}>+</Text>
-          </View>
-      </TouchableHighlight>
-    </View>
+          <ModalDropdown
+            options={['All Friends', 'Claire R.', 'John S.', 'Nate G.', 'Ella E.']}
+            defaultValue='All Friends'
+            showsVerticalScrollIndicator={true}
+            textStyle={{color: 'darkgray', fontSize: 28, marginTop: 23.5 }}>
+          </ModalDropdown>
+        </View>
+
+        <View style={{ flex: 6, justifyContent: 'flex-start'}}>
+          <FlatList
+            visible={this.state.memories.length!==0}
+            data={this.state.memories}
+            numColumns='3'
+            contentContainerStyle={this.container}
+            renderItem={({item}) => this._renderItem(item, {navigate})}
+
+            ItemSeparatorComponent={this.renderSeparator}
+          />
+
+    
+          <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'
+            onPress={() => navigate('AddMemory')} style={{position:'absolute', right:20, bottom:20, height:64, width:64, borderRadius:64/2}}> 
+              <View style={{alignItems: 'center', justifyContent:'center', flexDirection:'column', backgroundColor:'#EE4948',height:64, width:64, borderRadius:64/2, shadowColor: '#000000', shadowOffset: {width: 0, height: 4}, shadowRadius: 4, shadowOpacity: 0.7}}>
+                <Text style={{color:'#FFF', fontSize:32, marginBottom:5}}>+</Text>
+              </View>
+          </TouchableHighlight>
+        </View>
+
+      </View>
   )};
 
   renderSeparator = () => {
     return (
       <View
         style={{
-          height: 1,
-          width: '90%',
-          backgroundColor: '#999',
-          marginLeft: '5%',
-          marginRight: '5%'
+          marginTop: '3%',
+          marginLeft: '3%'
         }}
       />
     );
   };
+
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    marginBottom: 40
+}
 }
 
 export default MemoriesScreen;
