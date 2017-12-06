42b5f5f947a12344e0e1f63711cf61f202af3e42
aplim <aplim@stanford.edu>
2017-12-05 21:53:05 -0800

    ,
    essage

--- src/MemoriesScreen/MemoriesNavigator.js
+++ src/MemoriesScreen/MemoriesNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet, TouchableHighlight } from 'react-native';
 import { StackNavigator } from 'react-navigation'; // 1.0.0-beta.14
 import MemoriesScreen from './MemoriesScreen.js';
 import AddMemoryScreen from './AddMemoryScreen.js'
@@ -9,9 +9,14 @@ import MemoryDetailScreen from './MemoryDetailScreen.js'
 const RootNavigator = StackNavigator({
   Memories: {
     screen: MemoriesScreen,
-    navigationOptions: {
+    navigationOptions: ({navigation}) => ({
       headerTitle: 'Memories',
-    },
+      headerRight: 
+      <TouchableHighlight underlayColor='rgba(200,200,200,0.8)'
+        onPress={() => navigation.navigate('AddMemory')}>
+        <Text style={{color: '#007AFF', fontWeight: '300', fontSize: 38, marginRight: 13, marginBottom: 9}}>+</Text>
+      </TouchableHighlight>
+    }),
   },
 
   AddMemory: {
