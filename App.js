6114e0463a9ef93907f1be0f5f5af6e149eddbd6
rcole34 <rcole34@stanford.edu>
2018-02-01 14:03:29 -0800

    Access to profile screen through icon in top toolbar

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { View, Text, Button, FlatList, StyleSheet } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet, TouchableWithoutFeedback, Image } from 'react-native';
 import { StackNavigator } from 'react-navigation'; // 1.0.0-beta.14
 import DetailScreen from './src/HomeScreen/DetailScreen.js';
 import HomeScreen from './src/HomeScreen/HomeScreen.js'
 import AddFriendScreen from './src/HomeScreen/AddFriendScreen.js';
 // import ScheduleScreen from './ScheduleScreen/ScheduleScreen.js';
 import AddFriendInfoScreen from './src/HomeScreen/AddFriendInfoScreen.js';
+import ProfileScreen from "./src/ProfileScreen/ProfileScreen.js";
+import EditProfileScreen from "./src/ProfileScreen/EditProfileScreen.js";
 
 const RootNavigator = StackNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: ({navigation}) => ({
       headerTitle: 'Home',
+      headerLeft: null,
+      headerRight: 
+      <TouchableWithoutFeedback
+        onPress={() => navigation.navigate('Profile')}>
+          <View>
+            <Image source={require('./assets/icons/profile.png')} style={{tintColor: '#007AFF', height: 30, width: 30, marginRight: 20, marginBottom: 5}}/>
+          </View>
+      </TouchableWithoutFeedback>
     }),
   },
   Detail: {
@@ -21,6 +31,18 @@ const RootNavigator = StackNavigator({
     screen: AddFriendScreen,
     navigationOptions: ({navigation}) => ({
       title: "Add Connection",
+    }),
+  },
+  Profile: {
+    screen: ProfileScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Profile',
+    }),
+  },
+  EditProfile: {
+    screen: EditProfileScreen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Edit Profile',
     }),
   },
   // Schedule: {
